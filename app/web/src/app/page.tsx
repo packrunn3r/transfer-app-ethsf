b5837f43780316bc239e6d58b8e3897929142154
Leo Xuan <leoxuan@Leos-MacBook-Air-3.local>
2024-10-19 13:05:42 -0700

    First commit, added purple button and text field

--- app/web/src/app/page.tsx
+++ app/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SITE_DESCRIPTION, SITE_NAME } from '@/utils/site'
-
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
 export default function Home() {
   return (
     <>
@@ -8,6 +9,10 @@ export default function Home() {
 
       <div className='mt-4'>
         <p className='mb-4'>hello world</p>
+        <Button size='sm' className='bg-purple-500 hover:bg-purple-600'>
+          <a href='https://google.com'>Click me. First Shadcn component.</a>
+        </Button>
+        <Textarea placeholder='type something here' className='mt-4'></Textarea>
       </div>
     </>
   )
